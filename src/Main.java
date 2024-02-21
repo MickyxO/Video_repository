12a00feee43a481bf4aa7b7fb458811ad7b9ad80
C4TX0 <dmhgatogris333@hotmail.com>
2024-02-21 09:53:59 -0600

    Segundo commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(
                 "This is a video from Diego and Miguel.");
+        System.out.println("Este es un cambio");
     }
 }
